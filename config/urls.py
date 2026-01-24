9157d9aeb74a60835aa9da0a098ce8ba1a0bf296
Алексей <denaz2020@mail.ru>
2026-01-24 23:47:11 +0800

    add migrates

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("users/", include("users.urls", namespace="users")),
+    # path("users/", include("users.urls", namespace="users")),
     path("lms/", include("lms.urls", namespace="lms")),
 ]
