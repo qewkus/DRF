a1136f76e721fc47e9956ce1dd8344b01a5e34a2
Алексей <denaz2020@mail.ru>
2026-01-28 22:13:15 +0800

    add new serializer

--- lms/serializers.py
+++ lms/serializers.py
@@ -1,8 +1,10 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from lms.models import Course, Lesson
 
 
 class CourseSerializer(ModelSerializer):
+    lessons_count = SerializerMethodField()
+
     class Meta:
         model = Course
         fields = "__all__"
@@ -12,3 +14,15 @@ class LessonSerializer(ModelSerializer):
     class Meta:
         model = Lesson
         fields = "__all__"
+
+
+class CourseWithLessonsCountSerializer(ModelSerializer):
+    lessons_count = SerializerMethodField()
+    lessons = LessonSerializer(many=True, read_only=True)
+
+    def get_lessons_count(self, obj):
+        return obj.lessons.count()
+
+    class Meta:
+        model = Course
+        fields = ("id", "title", "description", "preview", "lessons_count", "lessons",)
