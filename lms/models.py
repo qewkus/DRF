321cda39c2e33a1352e54a14d4e1d42b47f7c712
Алексей <denaz2020@mail.ru>
2026-02-08 22:27:03 +0800

    add subscription in model

--- lms/models.py
+++ lms/models.py
@@ -47,3 +47,11 @@ class Lesson(models.Model):
     class Meta:
         verbose_name = "Урок"
         verbose_name_plural = "Уроки"
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ("user", "course")
