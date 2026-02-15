e11c9e5165b410410e3c1650af8357e8ef2a7ba8
Алексей <denaz2020@mail.ru>
2026-02-16 02:36:37 +0800

    in the Tasks file, a task is implemented that blocks the user

--- lms/tasks.py
+++ lms/tasks.py
@@ -1,10 +1,23 @@
 from celery import shared_task
 from django.core.mail import send_mail
+from django.utils import timezone
 
 from config.settings import EMAIL_HOST_USER
+from users.models import User
 
 
 @shared_task
 def send_course_update_email(email):
     """Рассылка на почту при обновлении курса"""
     send_mail('Обновление курса.', 'Ваш курс был обновлен.', from_email=EMAIL_HOST_USER, recipient_list=[email])
+
+
+@shared_task
+def blocked_inactive_user(user_id):
+    """Блокировка пользователя, если он не заходил более 30 дней."""
+    one_month_ago = timezone.now() - timezone.timedelta(days=30)
+    inactive_users = User.objects.filter(last_login__lt=one_month_ago, is_activ=True)
+
+    for user in inactive_users:
+        user.is_active = False
+        user.save()
