054543a1af694933ad5afcf0035d43de7d32b7a3
Алексей <denaz2020@mail.ru>
2026-03-19 23:29:50 +0800

    fixes code five

--- config/settings.py
+++ config/settings.py
@@ -159,7 +159,7 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 if "test" in sys.argv:
     DATABASES = {
         "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "NAME": BASE_DIR / "postgresql_psycopg2",
         }
     }
